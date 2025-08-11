d27dc27bb3e33643865f2a28b6281a8e925b616b
Nam Nguyen <nam.nguyen@saritasa.com>
2025-08-11 22:02:43 +0700

    Added sentry integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './global.css'
 import App from './App.tsx'
+import * as Sentry from "@sentry/react";
+
+const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN as string | undefined
+const SENTRY_ENV = import.meta.env.VITE_SENTRY_ENV as string | undefined
+
+if (SENTRY_DSN && SENTRY_ENV){
+    Sentry.init({
+        dsn: SENTRY_DSN,
+        sendDefaultPii: true,
+        integrations: [Sentry.browserTracingIntegration()],
+        tracesSampleRate: 1.0,
+        environment: SENTRY_ENV,
+        tracePropagationTargets: [
+            "localhost",
+            /^\/api\//,
+        ]
+    });
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
